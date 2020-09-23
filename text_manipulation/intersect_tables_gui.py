a078f98f9df45d19929cd5e04fbcdf133d4fbc1f
Olga Tsiouri <olgatsiouri@outlook.com>
2020-09-23 17:06:07 +0300

    Add files via upload

---
+++ text_manipulation/intersect_tables_gui.py
@@ -0,0 +1,24 @@
+# python3
+from gooey import *
+import pandas as pd
+# input parameters
+@Gooey(required_cols=2, program_name='intesect txt', header_bg_color= '#DCDCDC', terminal_font_color= '#DCDCDC', terminal_panel_color= '#DCDCDC')
+def main():
+  ap = GooeyParser(description= "exports the common lines of  2 txt files")
+  ap.add_argument("-tab1", "--table1", required=True, widget='FileChooser', help=" input txt file")
+  ap.add_argument("-out", "--output", required=True, widget='FileSaver', help="output txt file")
+  ap.add_argument("-tab2", "--table2", required=True, widget='FileChooser', help="txt file to match")
+  ap.add_argument("-header", "--header", required=True, help="txt header to select data for merge")
+  args = vars(ap.parse_args())
+# main
+  df1 = pd.read_csv(args['table1'], sep= "\t")
+  df2 = pd.read_csv(args['table2'], sep= "\t")
+  df_merge_col = pd.merge(df1, df2, on= args['header'], how='inner')
+# export
+  with open(args['output'], 'a') as f:
+    f.write(
+        df_merge_col.to_csv(header = True, index = False, sep= "\t", doublequote= False)
+    )
+
+if __name__ == '__main__':
+    main()
