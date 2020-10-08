4cfca68edbaeecd3f7e25a903075cf54e4577220
Olga Tsiouri <olgatsiouri@outlook.com>
2020-10-08 19:07:11 +0300

    tsv support

--- text_manipulation/print_lines_by_pattern_gui.py
+++ text_manipulation/print_lines_by_pattern_gui.py
@@ -5,8 +5,8 @@ import pandas as pd
 @Gooey(required_cols=4, program_name='print lines by pattern', header_bg_color= '#DCDCDC', terminal_font_color= '#DCDCDC', terminal_panel_color= '#DCDCDC')
 def main():
   ap = GooeyParser(description= "print lines that match a user specified pattern")
-  ap.add_argument("-in", "--input", required=True, widget='FileChooser', help=" input txt file")
-  ap.add_argument("-out", "--output", required=True, widget='FileSaver', help="output txt file")
+  ap.add_argument("-in", "--input", required=True, widget='FileChooser', help=" input txt/tsv file")
+  ap.add_argument("-out", "--output", required=True, widget='FileSaver', help="output txt/tsv file")
   ap.add_argument("-p", "--pattern", required=True, help="specify the pattern to print the lines that have it")
   ap.add_argument("-c", "--column", required=True, help="specify the column number to search for the pattern(starts from 0)")
   args = vars(ap.parse_args())
