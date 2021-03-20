4c4d7ce3d222ea204f349048dc35787ee734e788
Olga Tsiouri <olgatsiouri@outlook.com>
2021-03-20 03:51:19 +0200

    Update circuit_plotter_gui.py

--- synbio/circuit_plotter_gui.py
+++ synbio/circuit_plotter_gui.py
@@ -6,9 +6,9 @@ import synbiopython.genbabel as stdgen
 @Gooey(required_cols=3, program_name='circuit plotter', header_bg_color= '#DCDCDC', terminal_font_color= '#DCDCDC', terminal_panel_color= '#DCDCDC')
 def main():
     ap = GooeyParser()
-    ap.add_argument("-in", "--input", required=True, help="input circut components")
-    ap.add_argument("-reg", "--regulations", required=True, help="regulations of circut components(e.g activation)")
-    ap.add_argument("-plot", "--plot", required=True, widget='FileSaver', help="file to save the circut plot")
+    ap.add_argument("-in", "--input", required=True, help="input circuit components")
+    ap.add_argument("-reg", "--regulations", required=True, help="regulations of circuit components(e.g activation)")
+    ap.add_argument("-plot", "--plot", required=True, widget='FileSaver', help="file to save the circuit plot")
     args = vars(ap.parse_args())
 # main
     simplot = stdgen.SimpleDNAplot()
