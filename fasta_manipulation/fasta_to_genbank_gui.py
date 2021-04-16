be094353420dfc0332f72369ca0920a29d2750cd
Olga Tsiouri <olgatsiouri@outlook.com>
2021-04-16 03:37:35 +0300

    Update fasta_to_genbank_gui.py

--- fasta_manipulation/fasta_to_genbank_gui.py
+++ fasta_manipulation/fasta_to_genbank_gui.py
@@ -3,7 +3,7 @@ from gooey import *
 from Bio import SeqIO
 from Bio.Alphabet import generic_dna, generic_protein
 # imput parameters
-@Gooey(required_cols=2, program_name='genbank to fasta', header_bg_color= '#DCDCDC', terminal_font_color= '#DCDCDC', terminal_panel_color= '#DCDCDC')
+@Gooey(required_cols=2, program_name='fasta to genbank', header_bg_color= '#DCDCDC', terminal_font_color= '#DCDCDC', terminal_panel_color= '#DCDCDC')
 def main():
     ap = GooeyParser()
     ap.add_argument("-fa","--fasta", required=True, widget='FileChooser', help="input fasta file")
