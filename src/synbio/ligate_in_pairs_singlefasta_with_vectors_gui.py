cdf90136757d5cc0ba82e041c38808d01019edb2
Olga Tsiouri <olgatsiouri@outlook.com>
2022-02-16 01:56:26 +0200

    change the script to support file lists with duplicates

--- src/synbio/ligate_in_pairs_singlefasta_with_vectors_gui.py
+++ src/synbio/ligate_in_pairs_singlefasta_with_vectors_gui.py
@@ -5,6 +5,7 @@ from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 from Bio import SeqFeature
+import  pandas as pd
 # imput parameters
 @Gooey(required_cols=2, program_name='ligate in pairs single-fasta files with vectors', header_bg_color= '#DCDCDC', terminal_font_color= '#DCDCDC', terminal_panel_color= '#DCDCDC')
 def main():
