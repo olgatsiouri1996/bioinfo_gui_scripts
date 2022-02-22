5be73120d8c97518f977fdafa4e191414a7fd92b
Olga Tsiouri <olgatsiouri@outlook.com>
2022-02-22 21:01:46 +0200

    option to include a spacer between fasta records added

--- src/fasta_manipulation/merge_multifasta_to_singlefasta_gui.py
+++ src/fasta_manipulation/merge_multifasta_to_singlefasta_gui.py
@@ -9,6 +9,7 @@ def main():
     ap = GooeyParser()
     ap.add_argument("-mfa", "--multifasta", required=True, widget='FileChooser', help="input multi-fasta file to merge its sequences")
     ap.add_argument("-id", "--seqid", required=True, help="fasta header of the output file")
+    ap.add_argument("-sp", "--spacer", required=False, type=str, default="", help="nucleotides or aminoacids to add between the merged fasta sequences. Default: no sequence to add")
     ap.add_argument("-sfa", "--singlefasta", required=True, widget='FileSaver', help="output single-fasta file")
     args = vars(ap.parse_args())
 # main
@@ -16,7 +17,7 @@ def main():
     for record in SeqIO.parse(args['multifasta'], "fasta"):
         sequences.append(record.seq)
 # merge all sequences at 1 and create SeqRecord object
-    merged_seqs = SeqRecord(Seq(''.join(map(str,sequences))),id=args['seqid'],description="")
+    merged_seqs = SeqRecord(Seq(args['spacer'].join(map(str,sequences))),id=args['seqid'],description="")
 # export to fasta
     SeqIO.write(merged_seqs, args['singlefasta'], "fasta")
 
