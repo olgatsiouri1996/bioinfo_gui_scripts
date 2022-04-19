1eac770a377840c0918a2b9198c81d7e0f76ce24
Olga Tsiouri <olgatsiouri@outlook.com>
2022-04-19 20:36:55 +0300

    optimize for larger files, indentation  fixed

--- src/fasta_manipulation/extract_seqs_from_fasta_optimized_gui.py
+++ src/fasta_manipulation/extract_seqs_from_fasta_optimized_gui.py
@@ -6,14 +6,14 @@ from pyfaidx import Fasta
 # input parameters
 @Gooey(required_cols=2, program_name='extract seqs from fasta optimized', header_bg_color= '#DCDCDC', terminal_font_color= '#DCDCDC', terminal_panel_color= '#DCDCDC')
 def main():
-ap = GooeyParser()
-ap.add_argument("-in", "--input", required=True, widget="FileChooser", help="input multi-fasta file")
-ap.add_argument("-ids", "--ids", required=True, widget="FileChooser", help="file with fasta headers to retrieve the output fasta sequences")
-ap.add_argument("-out", "--output", required=False, widget="FileSaver", help="output multi-fasta file")
-ap.add_argument("-pro", "--program",type=int, default=1, required=False, help="choose to exract: 1) a multi-fasta file, 2) many single-fasta files")
-ap.add_argument("-dir", "--directory", required=False, type=str, widget="DirChooser", help="output directory to save the single-fasta files")
-args = vars(ap.parse_args())
-    # main
+    ap = GooeyParser()
+    ap.add_argument("-in", "--input", required=True, widget="FileChooser", help="input multi-fasta file")
+    ap.add_argument("-ids", "--ids", required=True, widget="FileChooser", help="file with fasta headers to retrieve the output fasta sequences")
+    ap.add_argument("-out", "--output", required=False, widget="FileSaver", help="output multi-fasta file")
+    ap.add_argument("-pro", "--program",type=int, default=1, required=False, help="choose to exract: 1) a multi-fasta file, 2) many single-fasta files")
+    ap.add_argument("-dir", "--directory", required=False, type=str, widget="DirChooser", help="output directory to save the single-fasta files")
+    args = vars(ap.parse_args())
+        # main
     # create function to split the input sequence based on a specific number of characters(60)
     def split_every_60(s): return [str(s)[i:i+60] for i in range(0,len(str(s)),60)]
     # import the txt file with headers you want to extract the sequence from the input fasta
