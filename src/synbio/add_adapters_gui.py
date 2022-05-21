481a857780211e21a9ad05af1b8ab15919930ad4
Olga Tsiouri <olgatsiouri@outlook.com>
2022-05-22 01:56:37 +0300

    add sequence of adapters instead of a fasta file

--- src/synbio/add_adapters_gui.py
+++ src/synbio/add_adapters_gui.py
@@ -4,30 +4,20 @@ from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 # imput parameters
-@Gooey(required_cols=4, program_name='add adapters to sequence', header_bg_color= '#DCDCDC', terminal_font_color= '#DCDCDC', terminal_panel_color= '#DCDCDC')
+@Gooey(required_cols=2, program_name='add adapters', header_bg_color= '#DCDCDC', terminal_font_color= '#DCDCDC', terminal_panel_color= '#DCDCDC')
 def main():
-    ap = GooeyParser()
-    ap.add_argument("-up", "--upstream", required=True, widget='FileChooser', help="upstream adapter(fasta format)")
-    ap.add_argument("-fa", "--fasta", required=True, widget='FileChooser', help="input single or multi fasta file with the sequence/s to add adapters")
-    ap.add_argument("-down", "--downstream", required=True, widget='FileChooser', help="downstream adapter(fasta format)")
-    ap.add_argument("-out", "--output", required=True, widget='FileSaver', help="output fasta file")
+    ap = GooeyParser(description = "add a left or right adapter or both in a single or multi-fasta file")
+    ap.add_argument("-i", "--input", required=True, widget='FileChooser',  help="input single or multi fasta file")
+    ap.add_argument("-l", "--left", required=False, default="", type=str, help="adapter to the left of the sequence")
+    ap.add_argument("-r", "--right", required=False, default="", type=str, help="adapter to the right of the sequence")
+    ap.add_argument("-o", "--output", required=True, widget='FileSaver',  help="output fasta file")
     args = vars(ap.parse_args())
-# main
-# first adapter
-    for record in SeqIO.parse(args['upstream'], "fasta"):
-        x = str(record.seq)
-# second adapter
-    for record in SeqIO.parse(args['downstream'], "fasta"):
-        z = str(record.seq)
-# sequence
+    # main
     records = [] # setup an empty list
-    for record in SeqIO.parse(args['fasta'], "fasta"):
-        y = str(record.seq)
-# merge
-        seqad = x + y + z
+    for record in SeqIO.parse(args['input'], "fasta"):
         # add this record to the list
-        records.append(SeqRecord(Seq(seqad),id=record.id,description=""))
-# export to fasta
+        records.append(SeqRecord(Seq(''.join([args['left'],str(record.seq),args['right']])),id=record.id,description=record.description))
+    # export to fasta
     SeqIO.write(records, args['output'], "fasta")
 
 if __name__ == '__main__':
