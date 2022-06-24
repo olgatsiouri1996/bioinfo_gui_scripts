c6f1168cef398eb76fb954534f8f0b11e61f2684
Olga Tsiouri <olgatsiouri@outlook.com>
2022-06-24 22:40:16 +0300

    widgets added

--- src/fasta_manipulation/complement_fasta_gui.py
+++ src/fasta_manipulation/complement_fasta_gui.py
@@ -4,9 +4,9 @@ from Bio import SeqIO
 # input parameters
 @Gooey(required_cols=2, program_name='complement fasta', header_bg_color= '#DCDCDC', terminal_font_color= '#DCDCDC', terminal_panel_color= '#DCDCDC')
 def main():
-    ap = argparse.ArgumentParser(description="complement the sequences of a multi or single-fasta file")
-    ap.add_argument("-in", "--input", required=True, help="input fasta file")
-    ap.add_argument("-out", "--output", required=True, help="output fasta file")
+    ap = GooeyParser(description="complement the sequences of a multi or single-fasta file")
+    ap.add_argument("-in", "--input", required=True, widget='FileChooser', help="input fasta file")
+    ap.add_argument("-out", "--output", required=True, widget='FileSaver', help="output fasta file")
     args = vars(ap.parse_args())
     # main
     sequences = []  # setup an empty list
--- src/fasta_manipulation/reverse_fasta_gui.py
+++ src/fasta_manipulation/reverse_fasta_gui.py
@@ -5,8 +5,8 @@ from Bio import SeqIO
 @Gooey(required_cols=2, program_name='reverse fasta', header_bg_color= '#DCDCDC', terminal_font_color= '#DCDCDC', terminal_panel_color= '#DCDCDC')
 def main():
     ap = GooeyParser(description="reverse the sequences of a multi or single-fasta file")
-    ap.add_argument("-in", "--input", required=True, help="input fasta file")
-    ap.add_argument("-out", "--output", required=True, help="output fasta file")
+    ap.add_argument("-in", "--input", required=True, widget='FileChooser', help="input fasta file")
+    ap.add_argument("-out", "--output", required=True, widget='FileSaver', help="output fasta file")
     args = vars(ap.parse_args())
     # main
     sequences = []  # setup an empty list
