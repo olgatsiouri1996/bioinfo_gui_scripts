9936e795966c7697d6b048fced1d1f06deec58d3
Olga Tsiouri <olgatsiouri@outlook.com>
2022-07-02 20:38:56 +0300

    %aa percentage added in fasta description, fasta description added in output tab file

--- src/sequence_statistics/proteins/subset_by_aa_gui.py
+++ src/sequence_statistics/proteins/subset_by_aa_gui.py
@@ -7,14 +7,15 @@ import pandas as pd
 def main():
     ap = GooeyParser()
     ap.add_argument("-in", "--input", required=True, widget='FileChooser', help="input fasta file")
-    ap.add_argument("-pro", "--program",type=int, default=1, required=False, help=" program to choose the output format 1) fasta, 2) txt file with headers 3) tab file with fasta headers and aa content percentage as columns. Defaults to 1)")
-    ap.add_argument("-max", "--max", required=False, default=101, help="max threshold of aa content, type = float")
+    ap.add_argument("-pro", "--program",type=int, default=1, required=False, help=" program to choose the output format 1) fasta with aa percentage in fasta description, 2) txt file with headers 3) tab file with fasta headers,aa content percentage and fasta description as columns. Defaults to 1)")
+    ap.add_argument("-max", "--max", required=False, default=100, help="max threshold of aa content, type = float")
     ap.add_argument("-min", "--min", required=False, default=0, help="min threshold of aa content, type = float")
     ap.add_argument("-aa", "--aa", required=True, help="aa to search the content for")
     ap.add_argument("-headers", "--headers", required=False, widget='FileSaver', help="file to save the output fasta headers")
     ap.add_argument("-fasta", "--fasta", required=False, widget='FileSaver', help="output fasta file")
-    ap.add_argument("-txt", "--txt", required=False, widget='FileSaver', help="output txt file with fasta headers and aa content percentage as columns")
+    ap.add_argument("-txt", "--txt", required=False, widget='FileSaver', help="output txt file with fasta headers, aa content percentage and fasta description as columns")
     args = vars(ap.parse_args())
+
     # create aa_content function
     def aa_content(seq):
         return round((seq.count(args['aa']) / len(seq)) * 100, 2)
@@ -25,7 +26,8 @@ def main():
     if program == 1:
         sequences = []  # setup an empty list
         for record in SeqIO.parse(args['input'], "fasta"):
-            if float(args['min']) < aa_content(record.seq) < float(args['max']):
+            if float(args['min']) <= aa_content(record.seq) <= float(args['max']):
+                record.description = ''.join(["%",args['aa']," ","content:"," ",str(aa_content(record.seq))," ",str(record.description).split(record.id)[1]])
                 # add this record to the list
                 sequences.append(record)
         # export to fasta
@@ -34,7 +36,7 @@ def main():
     elif program == 2:
         headers = []  # setup an empty list
         for record in SeqIO.parse(args['input'], "fasta"):
-            if float(args['min']) < aa_content(record.seq) < float(args['max']):
+            if float(args['min']) <= aa_content(record.seq) <= float(args['max']):
                 # add this record to the list
                 headers.append(record.id)
         # export to txt
@@ -43,21 +45,24 @@ def main():
                 filehandle.write('%s\n' % listitem)
     else:
         content = []
-        headers = []  # setup empty lists
+        headers = []
+        description = []  # setup empty lists
         for record in SeqIO.parse(args['input'], "fasta"):
-            if float(args['min']) < aa_content(record.seq) < float(args['max']):
+            if float(args['min']) <= aa_content(record.seq) <= float(args['max']):
                 # add this record to the list
                 headers.append(record.id)
                 content.append(aa_content(record.seq))
+                description.append(str(record.description).split(record.id)[1])
         # create data frame
         df = pd.DataFrame()
         df['protein_id'] = headers
-        df[''.join([args['aa'],"%"])] = content
+        df[''.join(["%",args['aa']])] = content
+        df['description'] = description
         # export
         with open(args['txt'], 'a') as f:
             f.write(
                 df.to_csv(header = True, index = False, sep= "\t", line_terminator= '\n')
             )
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
