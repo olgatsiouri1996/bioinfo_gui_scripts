f6ce0ef3e008d2a327b9fdde3d63089b0f9f4712
Olga Tsiouri <olgatsiouri@outlook.com>
2022-07-05 22:56:47 +0300

    Update count_length_from_fasta_gui.py

--- src/sequence_statistics/count_length_from_fasta_gui.py
+++ src/sequence_statistics/count_length_from_fasta_gui.py
@@ -5,26 +5,56 @@ import pandas as pd
 # input parameters
 @Gooey(required_cols=2, program_name= 'count length from fasta', header_bg_color= '#DCDCDC', terminal_font_color= '#DCDCDC', terminal_panel_color= '#DCDCDC')
 def main():
-  ap = GooeyParser(description="extract the id and length of sequence from a fasta or multifasta file")
-  ap.add_argument("-in", "--input", required=True, widget='FileChooser', help="input fasta file")
-  ap.add_argument("-out", "--output", required=True, widget='FileSaver', help="output txt file")
-  args = vars(ap.parse_args())
-# main
-  headers = []
-  lengths = [] # setup  empty lists
-  for record in SeqIO.parse(args['input'], "fasta"):
-        # add this record to the lists
-     lengths.append(len(record.seq))
-     headers.append(record.id)
-# create data frame
-  df = pd.DataFrame()
-  df['id'] = headers
-  df['length'] = lengths
-# export
-  with open(args['output'], 'a') as f:
-    f.write(
-        df.to_csv(header = True, index = False, sep= "\t", doublequote= False, line_terminator= '\n')
-    )
+    ap = GooeyParser(description="extract the id and length and fasta description of sequence from a fasta or multifasta file")
+    ap.add_argument("-in", "--input", required=True, widget='FileChooser', help="input fasta file")
+    ap.add_argument("-pro", "--program", type=int, default=1, required=False, help="program to choose: 1. output the length of all sequences, 2. output the length of the sequences that fall under a specific min,max threshold. Defaults to 1)")
+    ap.add_argument("-max", "--max", required=False, default=300, help="max number of sequence length. Default is 300")
+    ap.add_argument("-min", "--min", required=False, default=1, help="min number of sequence length. Default is 1")
+    ap.add_argument("-out", "--output", required=True, widget='FileSaver', help="output txt file")
+    args = vars(ap.parse_args())
+    # main
+    headers = []
+    lengths = []
+    descriptions = [] # setup  empty lists
+    # choose program
+    program = args['program']
+    # export for all sequences
+    if program == 1:
+        for record in SeqIO.parse(args['input'], "fasta"):
+            # add this record to the lists
+            lengths.append(len(record.seq))
+            headers.append(record.id)
+            descriptions.append(str(record.description).split(record.id)[1])
+        # create data frame
+        df = pd.DataFrame()
+        df['id'] = headers
+        df['length'] = lengths
+        df['description'] = descriptions
+        #print(df)
+        # export
+        with open(args['output'], 'a') as f:
+            f.write(
+                df.to_csv(header = True, index = False, sep= "\t", doublequote= False, escapechar='\\', line_terminator= '\n')
+            )
+    # subset based on max, min values
+    else:
+        for record in SeqIO.parse(args['input'], "fasta"):
+            if float(args['min']) <= len(record.seq) <= float(args['max']):
+                # add this record to the list
+                headers.append(record.id)
+                lengths.append(len(record.seq))
+                descriptions.append(str(record.description).split(record.id)[1])
+        # create data frame
+        df = pd.DataFrame()
+        df['id'] = headers
+        df['length'] = lengths
+        df['description'] = descriptions
+        # export
+        with open(args['output'], 'a') as f:
+            f.write(
+                df.to_csv(header = True, index = False, sep= "\t", escapechar='\\', line_terminator= '\n')
+            )
 
 if __name__ == '__main__':
     main()
+    
