5eb75fec78d5dc39f4035c7d28f6572c7b8f0f55
Olga Tsiouri <olgatsiouri@outlook.com>
2022-07-09 00:43:34 +0300

    he input multi-fasta is indexed instead of getting stored in memory

--- src/fasta_manipulation/select_longest_isoform_gui.py
+++ src/fasta_manipulation/select_longest_isoform_gui.py
@@ -6,8 +6,8 @@ from pyfaidx import Fasta
 @Gooey(required_cols=2, program_name='select longest isoform', header_bg_color= '#DCDCDC', terminal_font_color= '#DCDCDC', terminal_panel_color= '#DCDCDC')
 def main():
     ap = GooeyParser()
-    ap.add_argument("-in", "--input", required=True, help="input fasta")
-    ap.add_argument("-out", "--output", required=True, help="output fasta file")
+    ap.add_argument("-in", "--input", required=True, widget='FileChooser', help="input fasta file")
+    ap.add_argument("-out", "--output", required=True, widget='FileSaver', help="output fasta file")
     args = vars(ap.parse_args())
     # create function to split the input sequence based on a specific number of characters(60)
     def split_every_60(s): return [str(s)[i:i+60] for i in range(0,len(str(s)),60)]
