32fddcb1087070841e696398c8b43ebc02b3a0f0
Olga Tsiouri <olgatsiouri@outlook.com>
2022-08-16 17:27:36 +0300

    removed \r line endings from output files

--- src/fasta_manipulation/merge_multifasta_to_singlefasta_gui.py
+++ src/fasta_manipulation/merge_multifasta_to_singlefasta_gui.py
@@ -1,25 +1,31 @@
 # python3
 from gooey import *
-from Bio import SeqIO
-from Bio.SeqRecord import SeqRecord
-from Bio.Seq import Seq
+from pyfaidx import Fasta
+import sys
 # imput parameters
 @Gooey(required_cols=3, program_name='merge multifasta to singlefasta', header_bg_color= '#DCDCDC', terminal_font_color= '#DCDCDC', terminal_panel_color= '#DCDCDC')
 def main():
     ap = GooeyParser()
     ap.add_argument("-mfa", "--multifasta", required=True, widget='FileChooser', help="input multi-fasta file to merge its sequences")
-    ap.add_argument("-id", "--seqid", required=True, help="fasta header of the output file")
+    ap.add_argument("-id", "--seqid", required=True, type=str, help="fasta header of the output file")
     ap.add_argument("-sp", "--spacer", required=False, type=str, default="", help="nucleotides or aminoacids to add between the merged fasta sequences. Default: no sequence to add")
     ap.add_argument("-sfa", "--singlefasta", required=True, widget='FileSaver', help="output single-fasta file")
     args = vars(ap.parse_args())
-# main
-    sequences = []  # setup an empty list
-    for record in SeqIO.parse(args['multifasta'], "fasta"):
-        sequences.append(record.seq)
-# merge all sequences at 1 and create SeqRecord object
-    merged_seqs = SeqRecord(Seq(args['spacer'].join(map(str,sequences))),id=args['seqid'],description="")
-# export to fasta
-    SeqIO.write(merged_seqs, args['singlefasta'], "fasta")
+    # main
+    # create function to split the input sequence based on a specific number of characters(60)
+    def split_every_60(s): return [str(s)[i:i+60] for i in range(0,len(str(s)),60)]
+    # index fasta file
+    features = Fasta(args['multifasta'],as_raw=True)
+    # store all sequences to a list
+    sequences = [features[key][:] for key in features.keys()]
+    # merge all sequences at 1 and create SeqRecord object
+    merged_seqs = args['spacer'].join(sequences)
+    # export to fasta
+    sys.stdout = open(args['singlefasta'], 'a')
+    print(''.join([">",args['seqid']]).replace('\r',''))
+    print('\n'.join(split_every_60(merged_seqs)))
+    sys.stdout.close()
 
 if __name__ == '__main__':
     main()
+
