ebefae00207a8ad2c30950e2a4392e14d91393e3
olgatsiouri1996 <olgatsiouri@outlook.com>
2022-08-19 16:32:31 +0300

    Update split_fasta_into_chunks_gui.py

--- src/fasta_manipulation/split_fasta_into_chunks_gui.py
+++ src/fasta_manipulation/split_fasta_into_chunks_gui.py
@@ -11,22 +11,19 @@ def main():
     ap.add_argument("-step", "--step", required=True, type=int, help="step size for chunk creation, type = integer")
     ap.add_argument("-win", "--window", required=True, type=int, help="window size for chunk creation, type = integer")
     args = vars(ap.parse_args())
-# main
-# create function to split the input sequence based on a specific number of characters(60)
+    # main
+    # create function to split the input sequence based on a specific number of characters(60)
     def split_every_60(s): return [str(s)[i:i+60] for i in range(0,len(str(s)),60)]
-# create function to slice based on window and step
-    def slice_by_winstep(rec):
-        for i in range(0, features[rec][:].end - args['window'] + 1, args['step']):
-            print(''.join([">",rec,"_start_",str(i+1),"_stop_",str(i + args['window'])]).replace('\r',''))
-            print('\n'.join(split_every_60(features[rec][i:i + args['window']].seq)))
-        return
-# import multi or single-fasta file
+    # import multi or single-fasta file
     features = Fasta(args['input'])
-# iterate input headers to extract sequences and export as multi-fasta
+    # iterate input headers to extract sequences and export as multi-fasta
     sys.stdout = open(args['output'], 'a')
     for key in features.keys():
-        slice_by_winstep(key)
-    sys.stdout.close()	
+        # slice based on window and step
+        for i in range(0, features[key][:].end - args['window'] + 1, args['step']):
+            print(''.join([">",key,"_start_",str(i+1),"_stop_",str(i + args['window'])]).replace('\r',''))
+            print('\n'.join(split_every_60(features[key][i:i + args['window']].seq)))
+    sys.stdout.close()
 
 if __name__ == '__main__':
 	main()
