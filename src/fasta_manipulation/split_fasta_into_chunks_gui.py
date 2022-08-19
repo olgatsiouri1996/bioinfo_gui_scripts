8f56667338b0d0bb1b4c4939bc063eb29bbb13c0
Olga Tsiouri <olgatsiouri@outlook.com>
2022-08-19 03:17:49 +0300

    fixed output fasta headers

--- src/fasta_manipulation/split_fasta_into_chunks_gui.py
+++ src/fasta_manipulation/split_fasta_into_chunks_gui.py
@@ -5,28 +5,28 @@ from pyfaidx import Fasta
 # input parameters
 @Gooey(required_cols=4, program_name= 'split fasta into chunks optimized', header_bg_color= '#DCDCDC', terminal_font_color= '#DCDCDC', terminal_panel_color= '#DCDCDC')
 def main():
-	ap = GooeyParser()
-	ap.add_argument("-in", "--input", required=True, widget='FileChooser', help="input single or multi-fasta file")
-	ap.add_argument("-out", "--output", required=True, widget='FileSaver', help="output multi-fasta file")
-	ap.add_argument("-step", "--step", required=True, type=int, help="step size for chunk creation, type = integer")
-	ap.add_argument("-win", "--window", required=True, type=int, help="window size for chunk creation, type = integer")
-	args = vars(ap.parse_args())
+    ap = GooeyParser()
+    ap.add_argument("-in", "--input", required=True, widget='FileChooser', help="input single or multi-fasta file")
+    ap.add_argument("-out", "--output", required=True, widget='FileSaver', help="output multi-fasta file")
+    ap.add_argument("-step", "--step", required=True, type=int, help="step size for chunk creation, type = integer")
+    ap.add_argument("-win", "--window", required=True, type=int, help="window size for chunk creation, type = integer")
+    args = vars(ap.parse_args())
 # main
 # create function to split the input sequence based on a specific number of characters(60)
-	def split_every_60(s): return [str(s)[i:i+60] for i in range(0,len(str(s)),60)]
+    def split_every_60(s): return [str(s)[i:i+60] for i in range(0,len(str(s)),60)]
 # create function to slice based on window and step
-	def slice_by_winstep(rec):
-	    for i in range(0, features[rec][:].end - args['window'] + 1, args['step']):
+    def slice_by_winstep(rec):
+        for i in range(0, features[rec][:].end - args['window'] + 1, args['step']):
             print(''.join([">",rec,"_start_",str(i+1),"_stop_",str(i + args['window'])]).replace('\r',''))
-	        print('\n'.join(split_every_60(features[rec][i:i + args['window']].seq)))
-	    return
+            print('\n'.join(split_every_60(features[rec][i:i + args['window']].seq)))
+        return
 # import multi or single-fasta file
-	features = Fasta(args['input'])
+    features = Fasta(args['input'])
 # iterate input headers to extract sequences and export as multi-fasta
-	sys.stdout = open(args['output'], 'a')
-	for key in features.keys():
-	    slice_by_winstep(key)
-	sys.stdout.close()	
+    sys.stdout = open(args['output'], 'a')
+    for key in features.keys():
+        slice_by_winstep(key)
+    sys.stdout.close()	
 
 if __name__ == '__main__':
 	main()
