9861664da1f3180ae958b1f384b01e074683173b
Olga Tsiouri <olgatsiouri@outlook.com>
2022-09-07 20:20:44 +0300

    fix bug for fasta records with no fasta description

--- src/fasta_manipulation/fasta_to_tab_gui.py
+++ src/fasta_manipulation/fasta_to_tab_gui.py
@@ -1,31 +1,38 @@
 # python3
 from gooey import *
 import sys
-import pyfastx
+from pyfaidx import Fasta
 # input parameters
 @Gooey(required_cols=2, program_name='fasta to tab', header_bg_color= '#DCDCDC', terminal_font_color= '#DCDCDC', terminal_panel_color= '#DCDCDC')
 def main():
     ap = GooeyParser()
-    ap.add_argument("-in","--input", required=True, widget='FileChooser',help="input multi-fasta file(all fasta records should either have no fasta description or all of them should have fasta description)")
+    ap.add_argument("-in","--input", required=True, widget='FileChooser',help="input multi-fasta file")
     ap.add_argument("-pro", "--program", required=False, default=1, type=int, help="output to choose: 1) 2-column txt file with fasta identifiers and fasta sequences 2) 2-column txt file with full fasta headers and fasta sequences 3) 3-column txt file with fasta identifiers, fasta descriptions and fasta sequences")
     ap.add_argument("-out","--output", required=True, widget='FileSaver', help="output txt file")
     args = vars(ap.parse_args())
     # main
+    # index multi-fasta file
+    features = Fasta(args['input'])
     # choose program
     if args['program'] == 1:
         sys.stdout = open(args['output'], 'w')
-        for name,seq,comment in pyfastx.Fastx(args['input']):
-            print(name,seq,sep='\t')  
+        for key in features.keys():
+            print(key,features[key][:].seq,sep='\t')  
         sys.stdout.close()
     elif args['program'] == 2:
         sys.stdout = open(args['output'], 'w')
-        for name,seq,comment in pyfastx.Fastx(args['input']):
-            print(' '.join([name,comment]),seq,sep='\t')
+        for key in features.keys():
+            print(features[key].long_name,features[key][:].seq,sep='\t')
         sys.stdout.close()
     else:
         sys.stdout = open(args['output'], 'w')
-        for name,seq,comment in pyfastx.Fastx(args['input']):
-            print(name,comment,seq,sep='\t')  
+        for key in features.keys():
+            try:
+                description = str(features[key].long_name).split(' ',1)[1]
+            except IndexError:
+                description = ""
+     
+            print(key,description,features[key][:].seq,sep='\t')  
         sys.stdout.close()
 
 if __name__ == '__main__':
