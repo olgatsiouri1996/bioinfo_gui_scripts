26b7441d670c56ff1d9015b41985a71f2d03a009
Olga Tsiouri <olgatsiouri@outlook.com>
2022-09-25 18:32:22 +0300

    Update genomic_genbank_to_gene_with_annotations_gui.py

--- src/genbank/genomic_genbank_to_gene_with_annotations_gui.py
+++ src/genbank/genomic_genbank_to_gene_with_annotations_gui.py
@@ -10,7 +10,7 @@ def main():
 	ap.add_argument("-chr", "--chr", required=True, type=str, help="chromosome/scaffold/contig the gene is located")
 	ap.add_argument("-start", "--start", required=True, type=int, help="start of the gene in the chromosome/scaffold/contig")
 	ap.add_argument("-end", "--end", required=True, type=int, help="end of the gene in the chromosome/scaffold/contig")
-	ap.add_argument("-dir", "--directory", required=True, widget='DirChooser', type=str, help="directory to save the output genbank file. Default is the current directory")
+	ap.add_argument("-dir", "--directory", required=True, widget='DirChooser', type=str, help="directory to save the output genbank file")
 	args = vars(ap.parse_args())
 	# retrieve the gene with annotations from the genomic genbank file
 	for record in SeqIO.parse(args['input'], "genbank"):
