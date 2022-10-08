587e20aaba3aeedd8877f452982d42bed91ee813
Olga Tsiouri <olgatsiouri@outlook.com>
2022-10-09 00:18:36 +0300

    fasta descriptions added

--- src/fasta_manipulation/singlefastas_to_tab_gui.py
+++ src/fasta_manipulation/singlefastas_to_tab_gui.py
@@ -8,25 +8,31 @@ import pandas as pd
 def main():
 	ap = GooeyParser()
 	ap.add_argument("-dir", "--directory", required=True, type=str, widget='DirChooser', help="directory to search for fasta files")
-	ap.add_argument("-out","--output", required=True, widget='FileSaver', help="output txt file")
+	ap.add_argument("-out","--output", required=True, widget='FileSaver', help="output txt file with 3 columns without column names consisting of: id, description, sequence")
 	args = vars(ap.parse_args())
 # main
 	seqs = []
-	ids = [] # setup empty lists
+	ids = [] 
+	descriptions = [] # setup empty lists
 # import each fasta file from the working directory
 	for filename in sorted(os.listdir(os.chdir(args['directory']))):
 		if filename.endswith(".fa") or filename.endswith(".fasta"):
 			for record in SeqIO.parse(filename, "fasta"):
 				ids.append(record.id)
+				try:
+					descriptions.append(record.description.split(' ',1)[1])
+				except IndexError:
+					descriptions.append('')
 				seqs.append(record.seq)
 # put the 2 list in a data frame of 2 columns
 	dfasta = pd.DataFrame()
 	dfasta['id'] = ids
+	dfasta['desc'] = descriptions
 	dfasta['seq'] = seqs
 # export data frame to a tabular txt file
 	with open(args['output'], 'a') as f:
 	    f.write(
-	        dfasta.to_csv(header = False, index = False, sep= "\t", line_terminator= '\n')
+	        dfasta.to_csv(header = False, index = False, sep= "\t", lineterminator= '\n')
 	    )
 
 if __name__ == '__main__':
