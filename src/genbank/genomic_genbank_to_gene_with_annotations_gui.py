5d9155177a91e764a55ce6d1805963149dbad615
Olga Tsiouri <olgatsiouri@outlook.com>
2022-10-10 16:56:24 +0300

    added ability to choose qualifier

--- src/genbank/genomic_genbank_to_gene_with_annotations_gui.py
+++ src/genbank/genomic_genbank_to_gene_with_annotations_gui.py
@@ -10,6 +10,7 @@ def main():
 	ap.add_argument("-chr", "--chr", required=True, type=str, help="chromosome/scaffold/contig the gene is located")
 	ap.add_argument("-start", "--start", required=True, type=int, help="start of the gene in the chromosome/scaffold/contig")
 	ap.add_argument("-end", "--end", required=True, type=int, help="end of the gene in the chromosome/scaffold/contig")
+	ap.add_argument("-qual", "--qualifier", required=False, default='gene', type=str, widget='Dropdown', choices=['gene','locus_tag'], help="choose gene qualifier")
 	ap.add_argument("-dir", "--directory", required=True, widget='DirChooser', type=str, help="directory to save the output genbank file")
 	args = vars(ap.parse_args())
 	# retrieve the gene with annotations from the genomic genbank file
@@ -19,7 +20,7 @@ def main():
 	# retrieve the id of the gene to use as an output filename
 	for f in trimmed.features:
 		if f.type == 'gene' in f.qualifiers:
-			filename = f.qualifiers['locus_tag'][0]
+			filename = f.qualifiers[args['qualifier']][0]
 	# select directory to export to
 	os.chdir(args['directory'])
 	# export to genbank format
