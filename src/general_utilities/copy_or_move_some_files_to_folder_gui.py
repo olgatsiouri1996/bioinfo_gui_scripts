061c46c6eebbacec7f1e9d0380c4d964aa0e9a26
Olga Tsiouri <olgatsiouri@outlook.com>
2022-10-23 22:37:51 +0300

    Update copy_or_move_some_files_to_folder_gui.py

--- src/general_utilities/copy_or_move_some_files_to_folder_gui.py
+++ src/general_utilities/copy_or_move_some_files_to_folder_gui.py
@@ -6,7 +6,7 @@ import shutil
 @Gooey(required_cols=3, program_name='copy or move some files to folder', header_bg_color= '#DCDCDC', terminal_font_color= '#DCDCDC', terminal_panel_color= '#DCDCDC')
 def main():
     ap = GooeyParser()
-    ap.add_argument("-in", "--input", required=True, widget='FileChooser', help="input txt file with filenames to copy")
+    ap.add_argument("-in", "--input", required=True, widget='FileChooser', help="input 1-column txt file with filenames to copy")
     ap.add_argument("-ext", "--extension", required=False, type=str, default='.fasta', help="extension of the input files")
     ap.add_argument("-act", "--action", required=False, type=str, default='copy', choices=['copy','move'], help="copy or move selected files")
     ap.add_argument("-dir", "--directory", required=True, type=str, widget='DirChooser', help="directory with files to copy")
