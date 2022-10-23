8db3cd1dc90378243c9d4d8ce69ab7933a970b0e
Olga Tsiouri <olgatsiouri@outlook.com>
2022-10-23 21:02:39 +0300

    option to extract or remove some files

--- src/general_utilities/copy_or_move_some_files_to_folder_gui.py
+++ src/general_utilities/copy_or_move_some_files_to_folder_gui.py
@@ -3,11 +3,12 @@ from gooey import *
 import os
 import shutil
 # imput parameters
-@Gooey(required_cols=3, program_name='copy some files to folder', header_bg_color= '#DCDCDC', terminal_font_color= '#DCDCDC', terminal_panel_color= '#DCDCDC')
+@Gooey(required_cols=3, program_name='copy or move some files to folder', header_bg_color= '#DCDCDC', terminal_font_color= '#DCDCDC', terminal_panel_color= '#DCDCDC')
 def main():
     ap = GooeyParser()
     ap.add_argument("-in", "--input", required=True, widget='FileChooser', help="input txt file with filenames to copy")
     ap.add_argument("-ext", "--extension", required=False, type=str, default='.fasta', help="extension of the input files")
+    ap.add_argument("-act", "--action", required=False, type=str, default='copy', choices=['copy','move'], help="copy or move selected files")
     ap.add_argument("-dir", "--directory", required=True, type=str, widget='DirChooser', help="directory with files to copy")
     ap.add_argument("-out", "--output", required=True, type=str, widget='DirChooser', help="directory to save copied files")
     args = vars(ap.parse_args())
@@ -18,9 +19,15 @@ def main():
     input_files = [x.strip() + args['extension'] for x in input_files]
     # go to the directory where the files you want to copy are located
     os.chdir(args['directory'])
-    # copy each file
-    for file in input_files:
-        shutil.copy(file,args['output'])
+    # choose to copy or move files
+    if args['action'] == 'copy':
+        # copy each file
+        for file in input_files:
+            shutil.copy(file,args['output'])
+    else:
+        # copy each file
+        for file in input_files:
+            shutil.move(file,args['output']) 
 
 if __name__ == '__main__':
     main()
