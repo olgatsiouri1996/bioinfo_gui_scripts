904fa4aae2fd5d2eee428b2dc0cd6d0b605d33bd
Olga Tsiouri <olgatsiouri@outlook.com>
2022-10-29 02:22:37 +0300

    choose to move files for 1 or more extensions

--- src/general_utilities/copy_or_move_some_files_to_folder_gui.py
+++ src/general_utilities/copy_or_move_some_files_to_folder_gui.py
@@ -6,9 +6,10 @@ import shutil
 @Gooey(required_cols=3, program_name='copy or move some files to folder', header_bg_color= '#DCDCDC', terminal_font_color= '#DCDCDC', terminal_panel_color= '#DCDCDC')
 def main():
     ap = GooeyParser()
-    ap.add_argument("-in", "--input", required=True, widget='FileChooser', help="input 1-column txt file with filenames to copy")
+    ap.add_argument("-in", "--input", required=True, widget='FileChooser', help="input 1-column txt file with filenames to copy. if number is set to many then each filename should be followed by an extension e.g. file.txt")
     ap.add_argument("-ext", "--extension", required=False, type=str, default='.fasta', help="extension of the input files")
     ap.add_argument("-act", "--action", required=False, type=str, default='copy', choices=['copy','move'], help="copy or move selected files")
+    ap.add_argument("-num", "--number", required=False, type=str, default='one', choices=['one','many'], help="number of extensions selected files have") 
     ap.add_argument("-dir", "--directory", required=True, type=str, widget='DirChooser', help="directory with files to copy or move")
     ap.add_argument("-out", "--output", required=True, type=str, widget='DirChooser', help="directory to save copied or moved files")
     args = vars(ap.parse_args())
@@ -16,7 +17,11 @@ def main():
     # add filenames to copy
     with open(args['input'], 'r') as f:
         input_files = f.readlines()
-    input_files = [x.strip() + args['extension'] for x in input_files]
+    # select number of extensions
+    if args['number']=='one':
+        input_files = [x.strip() + args['extension'] for x in input_files]
+    else:
+        input_files = [x.strip() for x in input_files]
     # go to the directory where the files you want to copy are located
     os.chdir(args['directory'])
     # choose to copy or move files
