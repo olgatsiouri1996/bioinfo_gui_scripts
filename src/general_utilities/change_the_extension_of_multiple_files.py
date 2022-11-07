eda4f12eb45dd36d88391a59b2913d7c8e78e337
Olga Tsiouri <olgatsiouri@outlook.com>
2022-11-07 14:48:15 +0200

    Update change_the_extension_of_multiple_files.py

--- src/general_utilities/change_the_extension_of_multiple_files.py
+++ src/general_utilities/change_the_extension_of_multiple_files.py
@@ -6,13 +6,21 @@ import os
 def main():
     ap = GooeyParser()
     ap.add_argument("-dir", "--directory", required=True, type=str, widget='DirChooser', help="directory with files to change extension")
+    ap.add_argument("-in", "--input", required=False, type=str, default='.fsta', help="input file extension")
+    ap.add_argument("-num", "--number", required=False, type=str, default='one', choices=['one', 'many'], help="number of input file extensions. if set to many it can only be used to folders that contain only the files that you want to change the extension")
     ap.add_argument("-ext", "--extension", required=False, type=str, default='.fasta', help="extension to change into")
     args = vars(ap.parse_args())
     # main
     # change the file extension
-    for filename in sorted(os.listdir(os.chdir(args['directory']))):
-        base = os.path.splitext(filename)[0]
-        os.rename(filename, base + args['extension'])
+    if args['number']=='one':
+        for filename in sorted(os.listdir(os.chdir(args['directory']))):
+            if filename.endswith(args['input']):
+                base = os.path.splitext(filename)[0]
+                os.rename(filename, base + args['extension'])
+    else:
+        for filename in sorted(os.listdir(os.chdir(args['directory']))):
+            base = os.path.splitext(filename)[0]
+            os.rename(filename, base + args['extension'])
 
 if __name__ == '__main__':
     main()
