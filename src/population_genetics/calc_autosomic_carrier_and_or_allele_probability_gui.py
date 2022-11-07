d20a3cd408c2a3dc2685e552aa725340bd335500
Olga Tsiouri <olgatsiouri@outlook.com>
2022-11-07 15:21:20 +0200

    Create calc_autosomic_carrier_and_or_allele_probability_gui.py

---
+++ src/population_genetics/calc_autosomic_carrier_and_or_allele_probability_gui.py
@@ -0,0 +1,36 @@
+# python3
+from gooey import *
+import sys
+# input parameters
+@Gooey(required_cols=2, program_name='calculate the autosomic allele and/or carrier probability', header_bg_color= '#DCDCDC', terminal_font_color= '#DCDCDC', terminal_panel_color= '#DCDCDC')
+def main():
+    ap = GooeyParser(description='calculate the probability of an individual carrying at least 1 autosomic allele copy and/or being heterozygous, using the proportion of homozygous individuals in a given population')
+    ap.add_argument("-in", "--input", required=True, widget='FileChooser', help="1-column input txt file with the proportion of individuals homozygous for the specific allele")
+    ap.add_argument("-type", "--type", required=False, type=str, default='allele', choices=['allele', 'carrier', 'both'], help="type of calculation")
+    ap.add_argument("-out", "--output", required=True, widget='FileSaver', help="2 or 3-column tab-seperated output txt file with the probability of containing an allele and/or being heterozygous and the input column")
+    args = vars(ap.parse_args())
+    # convert 1-column txt file to list
+    with open(args['input'],'r') as f:
+        A = f.readlines()
+    A = [float(x.strip()) for x in A]
+    # choose and output based 
+    sys.stdout = open(args['output'],'w')
+    if args['type'] == 'allele':
+        print('probability_of_allele','proportion_of_homozygous_individuals',sep='\t',end='\n')
+        for i in A:
+            print("%.3f" % round(2*i**0.5-i,3),"%.3f" % round(i,3),sep='\t',end='\n')
+    elif args['type'] == 'carrier':
+        print('probability_of_carriers','proportion_of_homozygous_individuals',sep='\t',end='\n')
+        for i in A:
+            p = i**0.5
+            print("%.3f" % round(2*p*(1-p),3),"%.3f" % round(i,3),sep='\t',end='\n')
+    else:
+        print('probability_of_allele','probability_of_carriers','proportion_of_homozygous_individuals',sep='\t',end='\n')
+        for i in A:
+            p = i**0.5
+            print("%.3f" % round(2*i**0.5-i,3),"%.3f" % round(2*p*(1-p),3),"%.3f" % round(i,3),sep='\t',end='\n')
+
+    sys.stdout.close()
+
+if __name__ == '__main__':
+    main()
