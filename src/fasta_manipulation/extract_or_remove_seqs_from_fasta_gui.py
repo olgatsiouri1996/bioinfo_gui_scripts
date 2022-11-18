e082b41e24c66bb05825568b5c7d6e561761a7a9
Olga Tsiouri <olgatsiouri@outlook.com>
2022-11-18 02:32:50 +0200

    fixes for better performance memorywise

--- src/fasta_manipulation/extract_or_remove_seqs_from_fasta_gui.py
+++ src/fasta_manipulation/extract_or_remove_seqs_from_fasta_gui.py
@@ -12,15 +12,13 @@ def main():
     ap.add_argument("-ids", "--ids", required=True, widget='FileChooser', help="file with fasta headers to retrieve the output fasta sequences")
     ap.add_argument("-out", "--output", required=False, widget='FileSaver', help="output multi-fasta file")
     ap.add_argument("-dir", "--directory", required=False, type=str, widget='DirChooser',  help="output directory to save the single-fasta files.")
-    ap.add_argument("-pro", "--program",type=int, default=1, required=False, help="choose to: 1) extract sequences from a multi-fasta file, 2) extract many single-fasta files, 3) remove sequences from a multi-fasta file, 4) remove sequences and export to many single-fasta files . Defaults to 1")
+    ap.add_argument("-pro", "--program",type=int, default=1, required=False, choices=[1,2,3,4], widget='Dropdown', help="choose to: 1) extract sequences from a multi-fasta file, 2) extract many single-fasta files, 3) remove sequences from a multi-fasta file, 4) remove sequences and export to many single-fasta files . Defaults to 1")
     args = vars(ap.parse_args())
     # main
     # create function to split the input sequence based on a specific number of characters(60)
     def split_every_60(s): return [str(s)[i:i+60] for i in range(0,len(str(s)),60)]
     # import the txt file with headers you want to extract the sequence from the input fasta
-    with open(args['ids'], 'r') as f:
-        headers = f.readlines()
-    headers = [x.strip() for x in headers]
+    headers = (line.rstrip() for line in open(args['ids']))
     # import fasta file
     features = Fasta(args['input'])
     # choose program
@@ -30,7 +28,7 @@ def main():
         # iterate input headers to extract sequences and export as multi-fasta
         sys.stdout = open(args['output'], 'a')
         for header in headers:
-            print(''.join([">",features[str(header)].long_name]))
+            print(''.join([">",features[str(header)].long_name]).replace('\r',''))
             print('\n'.join(split_every_60(features[str(header)][:].seq)))
         sys.stdout.close()
     elif program == 2:
@@ -38,7 +36,7 @@ def main():
         os.chdir(args['directory'])
         for header in headers:
             sys.stdout = open(''.join([str(header),".fasta"]), 'a')
-            print(''.join([">",features[str(header)].long_name]))
+            print(''.join([">",features[str(header)].long_name]).replace('\r',''))
             print('\n'.join(split_every_60(features[str(header)][:].seq)))
             sys.stdout.close()
     elif program == 3:
@@ -49,11 +47,11 @@ def main():
         # export to 1 multi-fasta
         sys.stdout = open(args['output'], 'a')
         for key in keyslist:
-            print(''.join([">",features[str(key)].long_name]))
+            print(''.join([">",features[str(key)].long_name]).replace('\r',''))
             print('\n'.join(split_every_60(features[str(key)][:].seq)))
         sys.stdout.close()
     else:
-        # remove ids
+       # remove ids
         keyslist = list(features.keys())
         for header in headers:
             keyslist.remove(header)
@@ -61,7 +59,7 @@ def main():
         os.chdir(args['directory'])
         for key in keyslist:
             sys.stdout = open(''.join([str(key),".fasta"]), 'a')
-            print(''.join([">",features[str(key)].long_name]))
+            print(''.join([">",features[str(key)].long_name]).replace('\r',''))
             print('\n'.join(split_every_60(features[str(key)][:].seq)))
             sys.stdout.close()
 
