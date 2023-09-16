471c868daafc63085dd01aa517c751a2976cfa8b
Olga Tsiouri <olgatsiouri@outlook.com>
2023-09-17 02:01:54 +0300

    option to export by printing in the app removed  cause of unexpected bug

--- src/general_utilities/copy_or_move_some_files_to_folder_gui.py
+++ src/general_utilities/copy_or_move_some_files_to_folder_gui.py
@@ -1,16 +1,18 @@
 # python3
 from gooey import *
 import os
+import glob
 import shutil
 # imput parameters
-@Gooey(required_cols=3, program_name='copy or move some files to folder', header_bg_color= '#DCDCDC', terminal_font_color= '#DCDCDC', terminal_panel_color= '#DCDCDC')
+@Gooey(required_cols=2, program_name='copy or move some files to folder', header_bg_color= '#DCDCDC', terminal_font_color= '#DCDCDC', terminal_panel_color= '#DCDCDC')
 def main():
     ap = GooeyParser()
-    ap.add_argument("-in", "--input", required=True, widget='FileChooser', help="input 1-column txt file with filenames to copy. if number is set to many then each filename should be followed by an extension e.g. file.txt")
-    ap.add_argument("-ext", "--extension", required=False, type=str, default='.fasta', help="extension of the input files")
-    ap.add_argument("-act", "--action", required=False, type=str, default='copy', choices=['copy','move'], help="copy or move selected files")
-    ap.add_argument("-num", "--number", required=False, type=str, default='one', choices=['one','many'], help="number of extensions selected files have") 
     ap.add_argument("-dir", "--directory", required=True, type=str, widget='DirChooser', help="directory with files to copy or move")
+    ap.add_argument("-num", "--number", required=False, type=str, default='one', choices=['one','many'], help="number of extensions selected files have") 
+    ap.add_argument("-in", "--input", required=False, widget='FileChooser', help="input 1-column txt file with filenames to copy. Each filename should be followed by an extension e.g. file.txt")
+    ap.add_argument("-pat", "--pattern", required=False, type=str, default='.fasta', help="prefix or suffix of the input files")
+    ap.add_argument("-type", "--type", required=False, type=str, default='suffix', choices=['suffix','prefix'], help="select the files based on suffix or prefix")
+    ap.add_argument("-act", "--action", required=False, type=str, default='copy', choices=['copy','move'], help="copy or move selected files")
     ap.add_argument("-out", "--output", required=True, type=str, widget='DirChooser', help="directory to save copied or moved files")
     args = vars(ap.parse_args())
     # main
@@ -19,9 +21,12 @@ def main():
         input_files = f.readlines()
     # select number of extensions
     if args['number']=='one':
-        input_files = [x.strip() + args['extension'] for x in input_files]
+        if args['type']=='suffix':
+            input_files = glob.glob(os.path.join(args['directory'], '*'+args['pattern']))
+        else:
+            input_files = glob.glob(os.path.join(args['directory'], args['pattern']+'*'))
     else:
-        input_files = [x.strip() for x in input_files]
+        input_files = (x.rstrip() for x in input_files)
     # go to the directory where the files you want to copy are located
     os.chdir(args['directory'])
     # choose to copy or move files
