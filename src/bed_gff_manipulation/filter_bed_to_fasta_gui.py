2da8a09d2ab9a537f9a322737594c43ca019d4f7
Olga Tsiouri <olgatsiouri@outlook.com>
2023-09-23 18:52:33 +0300

    no usage of the sys module to remove bugs

--- src/bed_gff_manipulation/filter_bed_to_fasta_gui.py
+++ src/bed_gff_manipulation/filter_bed_to_fasta_gui.py
@@ -1,51 +1,61 @@
-# python3
 from gooey import *
-import sys
 from pyfaidx import Fasta
 import pandas as pd
 import warnings
+import textwrap
+
 # input parameters
-@Gooey(required_cols=3, program_name= 'filter bed to fasta', header_bg_color= '#DCDCDC', terminal_font_color= '#DCDCDC', terminal_panel_color= '#DCDCDC')
+@Gooey(
+    required_cols=3,
+    program_name='filter bed to fasta',
+    header_bg_color='#DCDCDC',
+    terminal_font_color='#DCDCDC',
+    terminal_panel_color='#DCDCDC',
+    default_size=(610,580)
+)
 def main():
     ap = GooeyParser()
-    ap.add_argument("-bed", "--bed", required=True, widget='FileChooser', help="input bed file(made with bedops, every feature in the \'.gff\' or \'.gff3\' file should have an \'ID\' tag in the \'attributes\' column)")
+    ap.add_argument("-bed", "--bed", required=True, widget='FileChooser',
+                    help="input bed file(made with bedops's gff2bed function, every feature in the '.gff' or '.gff3' file should have an 'ID' tag in the 'attributes' column)")
     ap.add_argument("-in", "--input", required=True, widget='FileChooser', help="input fasta file")
     ap.add_argument("-out", "--output", required=True, widget='FileSaver', help="output fasta file")
-    ap.add_argument("-fea", "--feature", required=False, default='gene',  type=str, help="specify the pattern to select the lines that have it")
-    ap.add_argument("-pro", "--program", required=False, default=1, type=int, help="program to choose: 1) filter the bed file before retrieving sequences, 2) do not filter")
+    ap.add_argument("-fea", "--feature", required=False, default='gene', choices=['gene','CDS','exon','intron','promoter'], type=str, help="specify the feature to collect sequences for")
+    ap.add_argument("-pro", "--program", required=False, default='filter the bed file by feature before retrieving sequences', type=str, choices=['filter the bed file by feature before retrieving sequences','do not filter'], help="program to choose")
     args = vars(ap.parse_args())
-    # main
-    # create function to split the input sequence based on a specific number of characters(60)
-    def split_every_60(s): return [str(s)[i:i+60] for i in range(0,len(str(s)),60)]
+
     # ignore warnings
     warnings.filterwarnings('ignore')
+
+    # Define the process_row function
+    def process_row(row, output_file, features):
+        chrom, start, end, ids, strand = row
+
+        if str(strand) == "+":
+            header = f">{ids} {chrom}:{int(start) + 1}-{end}".replace('\r', '')
+            seq = features[str(chrom)][int(start):end].seq
+        else:
+            header = f">{ids} {chrom}:{int(start) + 1}-{end} reverse complement".replace('\r', '')
+            seq = features[str(chrom)][int(start):end].reverse.complement.seq
+
+        output_file.write(header + '\n')
+        output_file.write(textwrap.fill(seq, width=60) + '\n')
+
     # import bed with no headers specified
-    df = pd.read_csv(args['bed'], sep= "\t", header=None)
+    df = pd.read_csv(args['bed'], sep="\t", header=None)
+
     # choose program
-    if args['program'] == 1:
+    if args['program'] == 'filter the bed file by feature before retrieving sequences':
         # select the rows containing the feature
-        bool2 = df.iloc[:, 7].str.contains(args['feature']) 
+        bool2 = df.iloc[:, 7].str.contains(args['feature'])
         df = df[bool2]
-    else:
-        pass
-    # convert each column to list
-    chrom = df.iloc[:,0].values.tolist()
-    start = df.iloc[:,1].values.tolist()
-    end = df.iloc[:,2].values.tolist()
-    ids = df.iloc[:,3].values.tolist()
-    strand = df.iloc[:,5].values.tolist()
+
     # import fasta file
     features = Fasta(args['input'])
-    # iterate all below lists in pairs
-    sys.stdout = open(args['output'], 'a')
-    for (a, b, c, d, e) in zip(ids, chrom, start, end, strand):
-        if str(e) == "+":
-            print(''.join([">",str(a)," ",str(b),":",str(int(c) + 1),"-",str(d)]).replace('\r', ''))
-            print('\n'.join(split_every_60(features[str(b)][int(c):int(d)].seq)))
-        else:
-            print(''.join([">",str(a)," ",str(b),":",str(int(c) + 1),"-",str(d)," ","reverse complement"]).replace('\r', ''))
-            print('\n'.join(split_every_60(features[str(b)][int(c):int(d)].reverse.complement.seq)))
-    sys.stdout.close()
+
+    # Process the DataFrame and write results to the output file
+    with open(args['output'], 'w') as output_file:
+        for _, row in df.iloc[:, [0, 1, 2, 3, 5]].iterrows():
+            process_row(row, output_file, features)
 
 if __name__ == '__main__':
     main()
