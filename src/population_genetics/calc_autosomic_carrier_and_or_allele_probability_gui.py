e078bd6602e1a085c48d46ff0c0e295509648a32
Olga Tsiouri <olgatsiouri@outlook.com>
2023-12-26 03:58:42 +0200

    Update calc_autosomic_carrier_and_or_allele_probability_gui.py

--- src/population_genetics/calc_autosomic_carrier_and_or_allele_probability_gui.py
+++ src/population_genetics/calc_autosomic_carrier_and_or_allele_probability_gui.py
@@ -1,6 +1,5 @@
 # python3
 from gooey import *
-import sys
 # input parameters
 @Gooey(required_cols=2, program_name='calculate the autosomic allele and/or carrier probability', header_bg_color= '#DCDCDC', terminal_font_color= '#DCDCDC', terminal_panel_color= '#DCDCDC')
 def main():
@@ -11,29 +10,27 @@ def main():
     ap.add_argument("-out", "--output", required=True, widget='FileSaver', help="3 or 4-column tab-seperated output txt file with the allele frequency, the probability of containing an allele and/or being heterozygous and the input column(for this or the other allele)")
     args = vars(ap.parse_args())
     # convert 1-column txt file to list
-    A = (float(line.rstrip()) for line in open(args['input']))
-    if args['allele'] == 'this':
-        pass
-    else:
-        A = ((1-(y**0.5))**2 for y in A)
-    # choose an output based on the calculation type
-    sys.stdout = open(args['output'],'w')
-    if args['type'] == 'allele':
-        print('probability_of_containing_allele','probability_of_homozygous_individuals','allele_frequency',sep='\t',end='\n')
-        for i in A:
-            print("%.3f" % round(2*i**0.5-i,3),"%.3f" % round(i,3),"%.3f" % round(i**0.5,3),sep='\t',end='\n')
-    elif args['type'] == 'carrier':
-        print('probability_of_carriers','probability_of_homozygous_individuals','allele_frequency',sep='\t',end='\n')
-        for i in A:
-            p = i**0.5
-            print("%.3f" % round(2*p*(1-p),3),"%.3f" % round(i,3),"%.3f" % round(p,3),sep='\t',end='\n')
-    else:
-        print('probability_of_containing_allele','probability_of_carriers','probability_of_homozygous_individuals','allele_frequency',sep='\t',end='\n')
-        for i in A:
-            p = i**0.5
-            print("%.3f" % round(2*i**0.5-i,3),"%.3f" % round(2*p*(1-p),3),"%.3f" % round(i,3),"%.3f" % round(p,3),sep='\t',end='\n')
+    probabilities = (float(line.rstrip()) for line in open(args['input']))
+    # Adjust probabilities based on allele choice
+    if args.allele == 'other':
+        probabilities = ((1 - (y ** 0.5)) ** 2 for y in probabilities)
 
-    sys.stdout.close()
+    # Choose an output based on the calculation type
+    with open(args.output, 'w') as output_file:
+        if args.type == 'allele':
+            output_file.write('probability_of_containing_allele\tprobability_of_homozygous_individuals\tallele_frequency\n')
+            for i in probabilities:
+                output_file.write(f"{round(2 * i**0.5 - i, 3)}\t{round(i, 3)}\t{round(i**0.5, 3)}\n")
+        elif args.type == 'carrier':
+            output_file.write('probability_of_carriers\tprobability_of_homozygous_individuals\tallele_frequency\n')
+            for i in probabilities:
+                p = i**0.5
+                output_file.write(f"{round(2 * p * (1 - p), 3)}\t{round(i, 3)}\t{round(p, 3)}\n")
+        else:
+            output_file.write('probability_of_containing_allele\tprobability_of_carriers\tprobability_of_homozygous_individuals\tallele_frequency\n')
+            for i in probabilities:
+                p = i**0.5
+                output_file.write(f"{round(2 * i**0.5 - i, 3)}\t{round(2 * p * (1 - p), 3)}\t{round(i, 3)}\t{round(p, 3)}\n")
 
 if __name__ == '__main__':
     main()
